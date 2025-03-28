cc8774430f1107d79151c1e558881fc4aaddd84a
ViniciusMikuni <vinicius.mikuni@gmail.com>
2025-03-27 17:48:38 -0700

    Add jetclass2, add label shift in case no pretraining is done

--- src/omnilearned/dataloader.py
+++ src/omnilearned/dataloader.py
@@ -115,7 +115,8 @@ def download_h5_files(base_url, destination_folder):
 class HEPDataset(Dataset):
     def __init__(self, file_paths, 
                  use_pid = False, pid_idx = -1,
-                 use_add = False,num_add = 4):
+                 use_add = False,num_add = 4,
+                 label_shift = 0):
         """
         Args:
             file_paths (list): List of file paths.
@@ -126,6 +127,7 @@ class HEPDataset(Dataset):
         self.use_add = use_add
         self.pid_idx = pid_idx
         self.num_add = num_add
+        self.label_shift = label_shift
         
         self.file_paths = file_paths
         self.file_indices = []  # [(file_index, sample_index), ...]
@@ -160,11 +162,10 @@ class HEPDataset(Dataset):
 
         sample['X'] = torch.tensor(f['data'][sample_idx], dtype=torch.float32)
         label = f['pid'][sample_idx]
-        sample['y'] = torch.tensor(label, dtype=torch.int64)
+        sample['y'] = torch.tensor(label - self.label_shift, dtype=torch.int64)
         
         if 'global' in f:
             sample['cond'] = torch.tensor(f['global'][sample_idx], dtype=torch.float32)
-
             
         if self.use_pid:
             sample['pid'] = sample['X'][:,self.pid_idx].int()
@@ -195,7 +196,7 @@ def load_data(dataset_name,path,
         raise ValueError(f"Dataset '{dataset_name}' not supported. Choose from {supported_datasets}.")
 
     if dataset_name == 'pretrain':
-        names = ['atlas','aspen', 'jetclass',  'h1']
+        names = ['atlas','aspen', 'jetclass', 'jetclass2', 'h1']
     else:
         names = [dataset_name]
         
@@ -214,10 +215,14 @@ def load_data(dataset_name,path,
             download_h5_files(url, dataset_path)
             
         file_list += [os.path.join(dataset_path, f) for f in os.listdir(dataset_path) if os.path.isfile(os.path.join(dataset_path, f))]
-
+        
+    #Shift labels if they are not used for pretrain
+    label_shift = {'jetclass':2,'aspen':12,'jetclass2':13}
+    
     data = HEPDataset(file_list,
                       use_pid=use_pid, pid_idx = pid_idx,
-                      use_add = use_add,num_add = num_add)
+                      use_add = use_add,num_add = num_add,
+                      label_shift = label_shift.get(dataset_name, 0))
 
     loader = DataLoader(data,batch_size=batch,
                         pin_memory=torch.cuda.is_available(),
