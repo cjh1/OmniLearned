7f491935cd364cbb5644e0a9c6f53c8c31249011
Chris Harris <cjh@lbl.gov>
2025-03-28 15:59:00 +0000

    Add basic tests for dataloader and train cli

---
+++ tests/cli/test_dataloader.py
@@ -0,0 +1,29 @@
+import tempfile
+from pathlib import Path
+
+from typer.testing import CliRunner
+
+from omnilearned.cli import app
+
+runner = CliRunner(mix_stderr=False)
+
+
+def test_download(test_dataset_name, test_dataset_file_paths):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_dir = Path(temp_dir)
+
+        result = runner.invoke(
+            app, ["dataloader", "--dataset", test_dataset_name, "--folder", temp_dir]
+        )
+        assert result.exit_code == 0, f"Error: {result.stdout} {result.stderr}"
+
+        # Assert the top exists
+        assert (
+            temp_dir / test_dataset_name
+        ).exists(), f"Top dataset not found in {temp_dir}"
+
+        # Assert the dataset paths exist
+        for path in test_dataset_file_paths:
+            assert (
+                temp_dir / path
+            ).exists(), f"Dataset path {path} not found in {temp_dir}"
---
+++ tests/cli/test_train.py
@@ -0,0 +1,44 @@
+import tempfile
+import re
+
+from typer.testing import CliRunner
+
+from omnilearned.cli import app
+
+runner = CliRunner(mix_stderr=False)
+
+
+def test_train(test_dataset_name, test_dataset_path):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = runner.invoke(
+            app,
+            [
+                "train",
+                "--epoch",
+                1,
+                "--path",
+                test_dataset_path,
+                "--dataset",
+                test_dataset_name,
+                "--output_dir",
+                temp_dir,
+            ],
+        )
+        assert result.exit_code == 0, f"Error: {result.stdout} {result.stderr}"
+
+        lines = result.stdout.split("\n")
+        # Remove empty lines
+        lines = [line for line in lines if line.strip()]
+
+        assert len(lines) > 0, "No output produced: {result.stderr}"
+
+        pattern = r"best loss: (\d+\.\d+)"
+        match = re.search(pattern, lines[-1])
+
+        assert (
+            match
+        ), f"Training Complete did not complete successfully: {result.stdout} {result.stderr}"
+        best_loss = float(match.group(1))
+        assert (
+            best_loss > 0 and best_loss < 0.5
+        ), f"Best loss out of range: {result.stdout} {result.stderr}"
